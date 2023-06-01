e215cdc3ca04d5cb990abc98d4780f85753b7808
udana <udana@denizli.bel.tr>
2023-06-01 16:09:49 +0300

    onboard page almost done

---
+++ lib/product/constants/color_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ColorConstants {
+  const ColorConstants._();
+
+  static const backgroundColor = Color(0xffFFFFFF);
+  static const onboardTitleTextColor = Color(0xff1F2937);
+  static const onboardSubtitleTextColor = Color(0xff4B5563);
+  static const onboardSkipTextColor = Color(0xff4B5563);
+  static const onboardIndicatorColor = Color(0xff32B768);
+  static const onboardUnselectedIndicatorColor = Color(0xffE6E6E6);
+}
---
+++ lib/product/constants/string_constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class StringConstants {
+  const StringConstants._();
+  static const projectNameText = 'Fodei';
+  static const onboardTitleTextFirst = 'Nearby restaurants';
+  static const onboardSubtitleTextFirst =
+      'You dont have to go far to find a good restaurant, we have provided all the restaurants that is near you';
+  static const onboardTitleTextSecond = 'Select the Favorites Menu';
+  static const onboardSubtitleTextSecond =
+      'Now eat well, dont leave the house,You can choose your favorite food only with one click';
+  static const onboardTitleTextThird = 'Good food at a cheap price';
+  static const onboardSubtitleTextThird =
+      'You can eat at expensive restaurants with affordable price';
+  static const onBoardSkipText = 'Skip';
+}
